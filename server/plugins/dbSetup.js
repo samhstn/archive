26dce22ffaec6565de2ad8eb513bfd68a0d42b4d
shouston3 <shouston3@me.com>
2016-10-02 19:37:54 +0100

    Adding a seperate db for testing

--- server/plugins/dbSetup.js
+++ server/plugins/dbSetup.js
@@ -1,8 +1,9 @@
 var pg = require('pg');
-var pgUrl = process.env.PG_URL;
+var test = process.env.NODE_ENV === 'test';
+var dbUrl = test ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 
 exports.register = function (server, options, next) {
-  var client = new pg.Client(pgUrl);
+  var client = new pg.Client(dbUrl);
   client.connect();
   server.app.client = client;
 
