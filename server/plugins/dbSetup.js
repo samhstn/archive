8baa0abd6c4bdf04306b97e847a2131fc7d0fc6e
shouston3 <shouston3@me.com>
2016-10-03 14:54:49 +0100

    Changing over to using a pool and extracting away the config to a dbConfig file

--- server/plugins/dbSetup.js
+++ server/plugins/dbSetup.js
@@ -1,16 +1,9 @@
 var pg = require('pg');
-var test = process.env.NODE_ENV === 'test';
-var dbUrl = test ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
-
-if (!process.env.NOT_HEORKU) {
-  console.log('for herokes: ', process.env.NOT_HEROKU, pg.defaults.ssl);
-  pg.defaults.ssl = true;
-}
+var config = require('../dbConfig.js');
 
 exports.register = function (server, options, next) {
-  var client = new pg.Client(dbUrl);
-  client.connect();
-  server.app.client = client;
+  var pool = new pg.Pool(config);
+  server.app.pool = pool;
 
   next();
 };
