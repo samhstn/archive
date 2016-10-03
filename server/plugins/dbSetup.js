ace73eecedb5b1323d367a480df8cf9bc6c81fbc
shouston3 <shouston3@me.com>
2016-10-03 09:02:03 +0100

    adding in pg.defaults.ssl true for heroku to connect to the database

--- server/plugins/dbSetup.js
+++ server/plugins/dbSetup.js
@@ -2,6 +2,13 @@ var pg = require('pg');
 var test = process.env.NODE_ENV === 'test';
 var dbUrl = test ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL;
 
+console.log(process.env.TEST_DATABASE_URL);
+
+if (!process.env.LOCAL) {
+  console.log(process.env.LOCAL, pg.defaults.ssl);
+  pg.defaults.ssl = true;
+}
+
 exports.register = function (server, options, next) {
   var client = new pg.Client(dbUrl);
   client.connect();
